41c389bef0962ffc2bf90114b023380265646daa
HelderSepu <heldersepu@gmail.com>
2016-10-13 22:18:37 -0400

    Work on the setup

--- install/setup.bat
+++ install/setup.bat
@@ -2,6 +2,7 @@
 @PROMPT $S
 :: clean up before starting
 @DEL *.exe
+@DEL *.pdb
 @CD ..
 @RD bin /s /q
 @RD obj /s /q
@@ -17,19 +18,32 @@
 @COLOR 07
 @SET VS="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 @IF NOT EXIST %VS% SET VS="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
+@IF NOT EXIST %VS% GOTO ERROR
 CALL %VS% CodeFetcher.sln /build Release
 @ECHO.
 
-:: clean up at the end
-@CD bin
-@CD release
-@DEL *.config /q
-@DEL *.xml /q
-@DEL *.pdb /q
+:: clean up 
+@DEL bin\release\*.config /q
+@DEL bin\release\*.xml /q
+
+:: ILMerge
+CALL packages\ILMerge.2.14.1208\tools\ILMerge.exe /wildcards /t:winexe /log:CodeFetcher.log /out:CodeFetcher.exe  bin\Release\CodeFetcher.exe  bin\Release\*.dll
+
 
 @ECHO.
 @ECHO.
 @COLOR 0A
-@MOVE *.exe ../../install
+@MOVE CodeFetcher.exe install
+@MOVE CodeFetcher.pdb install
+@ECHO.
+@PAUSE
+@EXIT
+
+
+:ERROR
+@COLOR 0C
+@ECHO.
+@ECHO.
+@ECHO  VISUAL STUDIO NOT FOUND!
 @ECHO.
 @PAUSE
