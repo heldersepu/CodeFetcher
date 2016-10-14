c1b11ed5b1bc87cf0a62a79dd615933ec12467f4
HelderSepu <HelderSepu@gmail.com>
2016-10-14 17:21:23 -0400

    fix the setup bat

--- install/setup.bat
+++ install/setup.bat
@@ -13,9 +13,12 @@
 @ECHO.
 @PAUSE
 
+@CLS
+@ECHO.
+@ECHO.
+@COLOR 0F
 
 :: Launch the Visual Studio Build
-@COLOR 07
 @SET VS="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 @IF NOT EXIST %VS% SET VS="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 @IF NOT EXIST %VS% GOTO ERROR
@@ -25,16 +28,15 @@ CALL %VS% CodeFetcher.sln /build Release
 :: clean up 
 @DEL bin\release\*.config /q
 @DEL bin\release\*.xml /q
+@DEL bin\release\*.pdb /q
+@DEL bin\release\*.dll /q
+@MOVE bin\release\CodeFetcher.exe install
 
-:: ILMerge
-CALL packages\ILMerge.2.14.1208\tools\ILMerge.exe /wildcards /t:winexe /log:CodeFetcher.log /out:CodeFetcher.exe  bin\Release\CodeFetcher.exe  bin\Release\*.dll
 
-
-@ECHO.
-@ECHO.
 @COLOR 0A
-@MOVE CodeFetcher.exe install
-@MOVE CodeFetcher.pdb install
+@ECHO.
+@ECHO.
+@ECHO  PROCESS COMPLETE!
 @ECHO.
 @PAUSE
 @EXIT
