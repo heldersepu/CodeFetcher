502f0bab89ad3d02ba6d7f95c821091afe52fa00
HelderSepu <HelderSepu@gmail.com>
2016-10-28 17:21:58 -0400

    finalize the tests

--- CodeFetcherTests/IndexTests.cs
+++ CodeFetcherTests/IndexTests.cs
@@ -12,41 +12,57 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CodeFetcher.Tests
 {
     [TestClass()]
     public class IndexTests
     {
+        Index index;
+        const int MAX_FILES = 10000;
+        const string TEST_STRING = "void function test";
+        public IndexTests()
+        {
+            var ini = new IniFile("", ".");
+            ini.HitsLimit = MAX_FILES;
+            index = new Index(ini);
+        }
+
         [TestMethod()]
         public void SearchTest()
         {
             using (ShimsContext.Create())
             {
-                ShimDirectory.GetFilesStringString = (dir, pattern) =>
+                ShimDirectoryInfo.AllInstances.GetFilesString = (dir, pattern) =>
                 {
-                    return new[] { @"C:\abc\test.txt" };
+                    var fis = new FileInfo[MAX_FILES];
+                    string name = Guid.NewGuid().ToString();
+                    for (int i = 0; i < MAX_FILES; i++)
+                        fis[i] = new FileInfo($"{name}_{i}.utest");
+                    return fis;
+                };
+                ShimFileInfo.AllInstances.LengthGet = (file) =>
+                {
+                    return (file.Exists)? GetFileSize(file.FullName): TEST_STRING.Length;
                 };
                 ShimFile.ReadAllTextString = (path) =>
                 {
-                    return "void function test";
+                    return TEST_STRING;
                 };
 
-                var ini = new IniFile("", ".");
-                var index = new Index(ini);
                 var worker = index.Initialize();
                 worker.RunWorkerAsync();
                 while (worker.IsBusy)
                     Thread.Sleep(100);
 
-                List<string> findings = new List<string>();
+                int findings = 0;
                 RunWorkerCompletedEventArgs completed = null;
                 var search = index.Search("function", null);
                 search.ProgressChanged +=
                     delegate (object sender, ProgressChangedEventArgs e)
                     {
-                        var t = (ListViewItem)e.UserState;
-                        findings.Add(t.SubItems[1].ToString());
+                        findings++;
                     };
                 search.RunWorkerCompleted +=
                     delegate (object sender, RunWorkerCompletedEventArgs e)
@@ -58,15 +74,26 @@ namespace CodeFetcher.Tests
                     Thread.Sleep(100);
 
                 if (completed.Error != null)
-                {
                     Assert.Fail(completed.Error.Message);
-                }
+                else if (findings < 1)
+                    Assert.Fail("NOTHING WAS FOUND");
                 else
-                {
-                    if (findings.Count < 1)
-                        Assert.Inconclusive();
-                }
+                    Assert.AreEqual(findings, MAX_FILES);
             }
         }
+
+        #region private methods
+        private static long GetFileSize(string file)
+        {
+            uint hosize;
+            uint losize = GetCompressedFileSizeW(file, out hosize);
+            return (long)hosize << 32 | losize;
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetCompressedFileSizeW([In, MarshalAs(UnmanagedType.LPWStr)] string lpFileName,
+           [Out, MarshalAs(UnmanagedType.U4)] out uint lpFileSizeHigh);
+        #endregion
+
     }
 }
