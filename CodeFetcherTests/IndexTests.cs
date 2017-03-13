fc5d4247c985e1de4e2d28c0eee46b8047fc9461
HelderSepu <HelderSepu@gmail.com>
2017-03-13 13:12:12 -0400

    Add more unit tests

--- CodeFetcherTests/IndexTests.cs
+++ CodeFetcherTests/IndexTests.cs
@@ -31,9 +31,9 @@ namespace CodeFetcher.Tests
             for (int i = 0; i < MAX_FILES; i++)
                 index.addContent(
                     LastWriteTime: DateTime.Now,
-                    type: "utest",
-                    name: $"{name}_{i}",
-                    path: i.ToString(),
+                    type: "uTest" + i.ToString(),
+                    name: $"Test_{name}_{i}",
+                    path: "Logs" + i.ToString(),
                     content: TEST_STRING,
                     exists: false);
             index.Close();
@@ -47,32 +47,34 @@ namespace CodeFetcher.Tests
         }
 
         [TestMethod()]
-        public void SearchTest()
+        public void SearchContentTest()
         {
-            int findings = 0;
-            RunWorkerCompletedEventArgs completed = null;
-            var search = index.Search("(FunctionX)", null);
-            search.ProgressChanged += delegate (object sender, ProgressChangedEventArgs e) { findings++; };
-            search.RunWorkerCompleted += delegate (object sender, RunWorkerCompletedEventArgs e) { completed = e; };
-            search.RunWorkerAsync();
-            while (search.IsBusy)
-                Thread.Sleep(100);
-            index.Cancel();
+            SearchTest("(FunctionX)", MAX_FILES);
+        }
 
-            if (completed.Error != null)
-                Assert.Fail(completed.Error.Message);
-            else if (findings < 1)
-                Assert.Fail("NOTHING WAS FOUND");
-            else
-                Assert.AreEqual(MAX_FILES, findings);
+        [TestMethod()]
+        public void SearchNameTest()
+        {
+            SearchTest("name:Test*", MAX_FILES);
+        }
+
+        [TestMethod()]
+        public void SearchTypeTest()
+        {
+            SearchTest("content:void AND type:uTest10", 1);
         }
 
         [TestMethod()]
         public void SearchPathTest()
         {
+            SearchTest("content:void AND path:Logs10", 1);
+        }
+
+        private void SearchTest(string query, int expected)
+        {
             int findings = 0;
             RunWorkerCompletedEventArgs completed = null;
-            var search = index.Search("content:void AND path:10", null);
+            var search = index.Search(query, null);
             search.ProgressChanged += delegate (object sender, ProgressChangedEventArgs e) { findings++; };
             search.RunWorkerCompleted += delegate (object sender, RunWorkerCompletedEventArgs e) { completed = e; };
             search.RunWorkerAsync();
@@ -85,7 +87,7 @@ namespace CodeFetcher.Tests
             else if (findings < 1)
                 Assert.Fail("NOTHING WAS FOUND");
             else
-                Assert.AreEqual(1, findings);
+                Assert.AreEqual(expected, findings);
         }
     }
 }
